ac592de8268bbd8dc1bf57e720acbd286150b90e
AdzerKI <silver_cat@list.ru>
2023-10-12 14:50:43 +0300

    hw 5

--- src/main/java/org/example/seminar_four/book/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.seminar_four.book;
-
-import java.util.List;
-
-public interface BookRepository {
-    Book findById(String id);
-    List<Book> findAll();
-}
--- src/test/java/seminar_four_tests/book/BookServiceTest.java
+++
@@ -1,7 +0,0 @@
-package seminar_four_tests.book;
-
-
-
-class BookServiceTest {
-
-}
